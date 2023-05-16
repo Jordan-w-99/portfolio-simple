bcb09c9d2d2992646e844b28287004442fc4ff91
Jordan <jordanw99@hotmail.co.uk>
2023-05-16 13:31:47 +0100

    adding uni work

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 async function loadProjects() {
-    const projects = (await(await fetch('./projects.json')).json()).projects;
+    const projects = await(await fetch('./projects.json')).json();
     return projects;
 }
 
-function createProjectDiv(project) {
+function createPersonalProjectDiv(project) {
     const name = project.name;
     const viewLink = project.view_link;
     const codeLink = project.code_link;
     const description = project.description;
 
-    const projectContainer = document.querySelector('.work-display-container');
+    const projectContainer = document.querySelector('.personal.work-display-container');
 
     const projectDiv = document.createElement('div');
     projectDiv.className = 'work-display';
@@ -83,9 +83,91 @@ function createProjectDiv(project) {
     // </div>
 }
 
+function createUniProjectDiv(project) {
+    const name = project.name;
+    const viewLink = project.view_link;
+    const grade = project.grade;
+    const description = project.description;
+
+    const projectContainer = document.querySelector('.uni.work-display-container');
+
+    const projectDiv = document.createElement('div');
+    projectDiv.className = 'work-display';
+
+    // TITLE
+    const projectTitle = document.createElement('h3');
+    projectTitle.innerText = name;
+    projectTitle.className = 'work-title';
+    projectDiv.appendChild(projectTitle);
+
+    // LINKS
+    const projectLinkContainer = document.createElement('div');
+    projectLinkContainer.className = 'work-link-container';
+
+    const projectViewButton = document.createElement('button');
+    projectViewButton.innerText = 'View';
+    projectViewButton.className = 'work-link';
+    projectViewButton.addEventListener('click', e => window.open(viewLink, '_blank'));
+
+    if(viewLink === "") {
+        projectViewButton.disabled = true;
+    }
+
+    const projectGradeDisplay = document.createElement('div');
+    projectGradeDisplay.innerText = `${grade}%`;
+    projectGradeDisplay.className = 'work-link';
+
+    projectLinkContainer.appendChild(projectViewButton);
+    projectLinkContainer.appendChild(projectGradeDisplay);
+
+    projectDiv.appendChild(projectLinkContainer);
+
+    // DESCRIPTION TOGGLE
+    const projectDescToggle = document.createElement('p');
+    projectDescToggle.className = 'work-desc-toggle';
+
+    const projectDescToggleSpan = document.createElement('span');
+    projectDescToggleSpan.innerText = 'Details';
+
+    projectDescToggle.appendChild(projectDescToggleSpan);
+
+    projectDiv.appendChild(projectDescToggle);
+
+    // DESCRIPTION OVERLAY
+    const blurOverlay = document.createElement('div');
+    blurOverlay.className = 'blur-overlay';
+
+    const projectDesc = document.createElement('p');
+    projectDesc.className = 'work-desc';
+    projectDesc.innerHTML = description;
+
+    blurOverlay.appendChild(projectDesc);
+
+    projectDiv.appendChild(blurOverlay);
+
+    // Finally add the project div to the container
+    projectContainer.appendChild(projectDiv);
+
+    // EXAMPLE HTML
+    // <div class="work-display">
+    //     <h3 class="work-tile">Asteroids</h3>
+    //     <div class="work-link-container">
+    //         <button onclick="window.open('https://jordan-w-99.github.io/Snake_3D/', '_blank')" class="work-link">View</button>
+    //         <div class="grade-display">74%</div>
+    //     </div>
+    //     <p class="work-desc-toggle"><span>Details</span></p>
+    //     <div class="blur-overlay">
+    //         <p class="work-desc">
+    //             An asteroids recreation in C++, using a basic OpenGL graphics library.
+    //         </p>
+    //     </div>
+    // </div>
+}
+
 async function populateProjects() {
     const projects = await loadProjects();
-    projects.forEach(project => createProjectDiv(project));
+    projects.personal_projects.forEach(project => createPersonalProjectDiv(project));
+    projects.uni_projects.forEach(project => createUniProjectDiv(project));
 }
 
 populateProjects();
